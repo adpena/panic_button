4220c5a1c524c8e481dfc8f44cdc1797822c7f7f
Alejandro Pena <adpena@gmail.com>
2019-07-26 13:42:01 -0500

    Preparing for pyinstaller --onefile build

--- main.py
+++ main.py
@@ -2,49 +2,65 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 from datetime import datetime
+import sys
+import time
+
 
 class PanicButton:
-  def __init__(self):
-    load_dotenv()
-    self.office_number = os.getenv("OFFICE_NUMBER")
-    self.twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-    self.twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
-    self.panic_dispatch = os.getenv("PANIC_DISPATCH")
-    self.panic_sender = os.getenv("PANIC_SENDER")
+    def __init__(self):
+        load_dotenv(
+            resource_path(("C:\\Users\\Shadow\\Documents\\GitHub\\panic_button\\.env"))
+        )
+        self.office_number = os.getenv("OFFICE_NUMBER")
+        self.twilio_account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+        self.twilio_auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+        self.panic_dispatch = os.getenv("PANIC_DISPATCH")
+        self.panic_sender = os.getenv("PANIC_SENDER")
 
-  def panic(self):
-    print("##############################")
-    print("### PANIC BUTTON ACTIVATED ###")
-    print("##############################\n")
-    panic_message = input("Press ENTER to skip or type message:\t")
-    if panic_message == "":
-      # print(f"NEED HELP NOW @ {self.office_number}")
-      self.send_panic(panic_message)
-      exit()
-    else:
-      # print(f"NEED HELP NOW @ {self.office_number}")
-      # print(f"MESSAGE: {panic_message}")
-      self.send_panic(panic_message)
-      exit()
+    def panic(self):
+        print("##############################")
+        print("### PANIC BUTTON ACTIVATED ###")
+        print("##############################\n")
+        panic_message = input("Press ENTER to skip or type message:\t")
+        if panic_message == "":
+            self.send_panic(panic_message)
+        else:
+            self.send_panic(panic_message)
 
-  def send_panic(self, panic_message=""):
-    datetime_sent = datetime.now().strftime("%m/%d/%Y @ %H:%M:%S")
-    if panic_message != "":
-      client = Client(self.twilio_account_sid, self.twilio_auth_token)
-      message = client.messages.create(
-      to=self.panic_dispatch, 
-      from_=self.panic_sender,
-      body=f"PANIC BUTTON: Need help now at {self.office_number}.\nMESSAGE: {panic_message}\nSENT: {datetime_sent}")
-      print(f"Message sent to {self.panic_dispatch} - help is on its way!")
-    else:
-      client = Client(self.twilio_account_sid, self.twilio_auth_token)
-      message = client.messages.create(
-      to=self.panic_dispatch, 
-      from_=self.panic_sender,
-      body=f"PANIC BUTTON: Need help now at {self.office_number}.\nMESSAGE: N/A\nSENT: {datetime_sent}")
-      print(f"Message sent to {self.panic_dispatch} - help is on its way!")
+    def send_panic(self, panic_message=""):
+        datetime_sent = datetime.now().strftime("%m/%d/%Y @ %H:%M:%S")
+        client = Client(self.twilio_account_sid, self.twilio_auth_token)
+        if panic_message != "":
+            message = client.messages.create(
+                to=self.panic_dispatch,
+                from_=self.panic_sender,
+                body=f"PANIC BUTTON: Need help now @ {self.office_number}.\nMESSAGE: {panic_message}\nSENT: {datetime_sent}",
+            )
+            print(f"Message sent to {self.panic_dispatch} - help is on its way!\n")
+        else:
+            message = client.messages.create(
+                to=self.panic_dispatch,
+                from_=self.panic_sender,
+                body=f"PANIC BUTTON: Need help now @ {self.office_number}.\nMESSAGE: N/A\nSENT: {datetime_sent}",
+            )
+            print(f"Message sent to {self.panic_dispatch} - help is on its way!\n")
+        print("ALERT: This window will close in 5 seconds...")
+        time.sleep(5)
+        sys.exit()
 
 
-if __name__ == '__main__':
-  panic_button = PanicButton()
-  panic_button.panic()
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+if __name__ == "__main__":
+    panic_button = PanicButton()
+    panic_button.panic()
+    k = input("press close to exit")
